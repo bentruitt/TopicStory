ac7926e0a1c0c1e83e74bac0042a68e289673f6b
Scott Sullivan <scottmsul@gmail.com>
2017-02-26 13:01:46 -0700

    updated clean.sql to match current schema

--- sql/clean.sql
+++ sql/clean.sql
@@ -1,8 +1,6 @@
-DROP TABLE cosine_similarities;
-DROP TABLE article_labels;
 DROP TABLE articles;
 DROP TABLE links;
 DROP TABLE visits;
+DROP TABLE crawls;
 DROP TABLE sources;
-DROP TABLE robots;
 DROP TABLE urls;
